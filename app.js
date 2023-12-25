0829b5970c39a1b6f0a09326a006c049a874eece
muhammad45401 <saadjani3333@gmail.com>
2023-12-25 23:39:49 +0500

    function

---
+++ app.js
@@ -0,0 +1,123 @@
+document.write("Q:-1"+"<br> <br>")
+
+
+function date (){
+    var now = new Date()
+    document.write(now)
+}
+date();
+document.write("<br>")
+document.write("Q:-2"+"<br> <br>")
+
+// function name(){
+//     var fName = prompt("Enter your first name..?")
+//     var lName = prompt("Enter your last name ..?")
+//     var fullname = fName+lName
+//     document.write(fullname)
+// }
+
+// name()
+
+document.write("<br>"+"Q:-3"+"<br> <br>")
+
+
+// function sum(){
+//     var num1 = +prompt("Enter your first number..?")
+//     var num2 = +prompt("Enter your second number ..?")
+//     var sum = num1+num2
+//     document.write(sum)
+// }
+
+// sum()
+
+document.write("<br>"+"Q:-4"+"<br> <br>")
+// function calculator(){
+//     var opt = prompt("Enter the operator exp - + * /")
+//     var num3 = +prompt("Enter your first number..?")
+//     var num4 = +prompt("Enter your second number ..?")
+//     if ( opt === "-"){
+//         document.write(num3 - num4)
+//     }else if (opt === "+"){
+//         document.write(num3 + num4)
+//     }else if (opt === "*"){
+//         document.write(num3 * num4)
+//     }else if (opt === "/"){
+//         document.write(num3 / num4)
+//     }else alert("plase enter the right opertor")
+    
+// }
+
+// calculator()
+document.write("<br>"+"Q:-5"+"<br> <br>")
+
+// function sqr(){
+//     var sqr1 =prompt("plase enter a number..!")
+//      document.write(Math.sqrt(sqr1)+"<br>")
+// }
+// sqr()
+
+document.write("<br>"+"Q:-6"+"<br> <br>")
+// function factoril(number){
+//     var fac = 1;
+//     if ( number == 0){
+//         document.write("The factoril of " + number + " is 1")
+//     }else if (number < 0 ){
+//         document.write("The factoril of -ive number  is not possible")
+//     }else{
+//         for (var i=1; i<=number;i++){
+//             fac = fac*i
+            
+//         }
+//     }document.write(fac)
+// }
+// factoril(prompt("enter a number"))
+
+document.write("<br>"+"Q:-7"+"<br> <br>")
+
+// function count(){
+//     var cnt1 = prompt("Enter a first number")
+//     var cnt2 = prompt("Enter a second number ")
+//     for( var i=cnt1; i<=cnt2; i++){
+//         document.write(i)  
+//     }
+  
+// }
+
+// count()
+document.write("<br>"+"Q:-8"+"<br> <br>")
+
+function out(){
+    document.write("Hypotenuse =")
+    function iner(){
+      document.write(" Base + Perpendicular")
+
+
+    }
+    iner()
+}
+out()
+
+document.write("<br>"+"Q:-8"+"<br> <br>")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
